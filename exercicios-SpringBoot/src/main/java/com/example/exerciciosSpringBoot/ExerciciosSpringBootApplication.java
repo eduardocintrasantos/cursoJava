f055a1abe545b39de7f6ec719e6bfafea5578382
Eduardo Cintra dos Santos <38438615+eduardocintrasantos@users.noreply.github.com>
2020-10-12 10:57:35 -0300

    SpringBoot1
    Inicio modulo de SpringBoot

---
+++ exercicios-SpringBoot/src/main/java/com/example/exerciciosSpringBoot/ExerciciosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.exerciciosSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosSpringBootApplication.class, args);
+	}
+
+}
---
+++ exercicios-SpringBoot/src/test/java/com/example/exerciciosSpringBoot/ExerciciosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exerciciosSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
