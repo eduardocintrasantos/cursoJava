486f701e4cc6f806ce146b88592491e33538eeb4
Eduardo Cintra dos Santos <38438615+eduardocintrasantos@users.noreply.github.com>
2020-10-16 10:26:45 -0300

    JavaFXContador
    Contador em JavaFX

--- exercicios-SpringBoot/src/main/java/com/example/exerciciosSpringBoot/controllers/ClienteController.java
+++ exercicios-SpringBoot/src/main/java/com/example/exerciciosSpringBoot/controllers/ClienteController.java
@@ -14,4 +14,9 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
+	
+	@GetMapping()
+	public Cliente obterClientePorId1(int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
+	}
 }
---
+++ exercicios-javafx/src/basico/PrimeiroFX.java
@@ -0,0 +1,43 @@
+package basico;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+//import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class PrimeiroFX extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Button botaoA = new Button("A");
+		Button botaoB = new Button("B");
+		Button botaoC = new Button("C");
+		
+		botaoC.setOnAction(e -> {
+			System.exit(0);
+		});
+		
+		// HBox box = new HBox(); Cria a box na horizontal
+		VBox box = new VBox();
+		box.setSpacing(10);
+		box.setAlignment(Pos.CENTER);
+		
+		box.getChildren().add(botaoA);
+		box.getChildren().add(botaoB);
+		box.getChildren().add(botaoC);
+		
+		Scene unicaCena = new Scene(box, 100, 150);
+		
+		primaryStage.setScene(unicaCena);	
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
